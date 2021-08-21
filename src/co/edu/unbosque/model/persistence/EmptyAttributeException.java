aaf500e94b918df3bf64da5baec5be68364e7326
Felipe Segura <fsegurar@unbosque.edu.co>
2021-08-21 15:04:44 -0500

    read File metod

---
+++ src/co/edu/unbosque/model/persistence/EmptyAttributeException.java
@@ -0,0 +1,8 @@
+package co.edu.unbosque.model.persistence;
+
+public class EmptyAttributeException extends Exception {
+
+	public EmptyAttributeException() {
+		super("neighborhood no asignado");
+	}
+}
--- src/co/edu/unbosque/model/persistence/Manager.java
+++ src/co/edu/unbosque/model/persistence/Manager.java
@@ -1,16 +1,64 @@
 package co.edu.unbosque.model.persistence;
 
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
+
+import com.opencsv.CSVParser;
+import com.opencsv.CSVParserBuilder;
+import com.opencsv.CSVReader;
+import com.opencsv.CSVReaderBuilder;
+import com.opencsv.exceptions.CsvValidationException;
 
 public class Manager {
 	
 	private ArrayList<Pet> pet;
 	private ArrayList<Pet> dato;
+	private ArrayList<Pet> omit;
+	private CSVReader csvReader;
+	private FileReader csvFile;
 	
 	public Manager() {
 		pet = new ArrayList<>();
 		dato = new ArrayList<>();
 		
 	}
+	
+	public void readFile(String file){
+		boolean dangerous = false;
+		try {
+			csvFile = new FileReader(file);
+			CSVParser conPuntoYComa = new CSVParserBuilder().withSeparator(';').build();
+			csvReader = new CSVReaderBuilder(csvFile).withCSVParser(conPuntoYComa).build();
+			String[] fila = null;
+			csvReader.readNext();
+			while ((fila = csvReader.readNext()) != null) {
+				try {
+					if(fila[3].equals("NO")) {
+						dangerous = false;
+					} else if(fila[3].equals("SI")) {
+						dangerous = true;
+					}
+					
+					if (fila[4].equals(null)) {
+						throw new EmptyAttributeException();
+					}
+					pet.add(new Pet("NO ASIGNADO", Long.parseLong(fila[0]), fila[1], fila[2], dangerous, fila[4]));
+				
+				}catch (EmptyAttributeException e) {
+					omit.add(new Pet("NO ASIGNADO", Long.parseLong(fila[0]), fila[1], fila[2], dangerous, "No Asignado"));
+				}catch (NumberFormatException e) {
+					omit.add(new Pet("NO ASIGNADO", 0000000000, fila[1], fila[2], dangerous, fila[4]));
+				}
+			}
+			csvFile.close();
+			csvReader.close();
+			
+		} catch(IOException e) {
+				
+		}catch (CsvValidationException e) {
+				
+	}
+}
 
 }
